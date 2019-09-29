2bf320a0cbc0f3e0ff3b57d232dc35e36b6bed6e
fathur rohman <rahmanboy987@gmail.com>
2019-09-29 19:11:23 +0700

    publish file index

---
+++ _config/config.php
@@ -0,0 +1,18 @@
+<?php
+date_default_timezone_set("Asia/Jakarta");
+session_start();
+
+$con = mysqli_connect('www.gabutin.site', 'gabutins', 'qw0147313060', 'gabutins_pemweb');
+if (mysqli_connect_errno()) {
+    echo mysqli_connect_error();
+}
+
+function base_url($url = null)
+{
+    $base_url = "http://pemweb.gabutin.site/lohkoklahkokloh";
+    if ($url != null) {
+        return $base_url . "/" . $url;
+    } else {
+        return $base_url;
+    }
+}
