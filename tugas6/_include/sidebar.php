0f3aeca9a6f5579f670a92231b9ffe0dbf41174e
fathur rohman <rahmanboy987@gmail.com>
2019-10-04 15:55:45 +0700

    Update sidebar.php

--- tugas6/_include/sidebar.php
+++ tugas6/_include/sidebar.php
@@ -6,7 +6,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">PemWeb Tugas<sup>5</sup></div>
+        <div class="sidebar-brand-text mx-3">PemWeb Tugas<sup>6</sup></div>
     </a>
 
     <!-- Divider -->
